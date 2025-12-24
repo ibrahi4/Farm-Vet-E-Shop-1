cca5b3af4e14ccddec67de83bdc5489158774da4
ibrahi4 <ibrahim1342000@gmail.com>
2025-12-24 13:09:02 +0200

    fix hero section

--- src/pages/admin/AdminDashboard.jsx
+++ src/pages/admin/AdminDashboard.jsx
@@ -273,7 +273,7 @@ export default function AdminDashboard() {
           </h1>
           <button
             onClick={fetchAllData}
-            className={`flex items-center gap-3 px-6 py-3 rounded-xl shadow ${
+            className={`flex items-center gap-2.5 px-1.5 py-2 rounded-xl shadow ${
               isDark
                 ? "bg-gradient-to-r from-green-600 to-blue-600 hover:from-green-700 hover:to-blue-700"
                 : "bg-emerald-500 text-white hover:bg-emerald-600"
